653d4128b3c685432bd2e440fc597d3ca29bf360
willpags1 <willpags1@gmail.com>
2020-12-04 09:39:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
 
-    response = requests.get('https://httpbin.org/ip')
+response = requests.get('https://httpbin.org/ip')
 
 print('Your IP is {0}'.format(response.json()['origin']))
